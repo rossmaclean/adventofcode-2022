c20f003d7be783a365f8a9e76e318b5441e6ecb4
Ross MacLean <ross@rossmac.co.uk>
2022-11-30 19:59:55 +0000

    Initial commit.

---
+++ challenges/1/main_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import main
+
+
+class TestMain(unittest.TestCase):
+
+  def test_add(self):
+    expected = 15
+
+    self.assertEqual(expected, main.add(10, 5))
+
+
+if __name__ == '__main__':
+  unittest.main()
